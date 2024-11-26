e52142850e425e2c931a449aa071c88dfcd8cda8
hdcola <gh@hdcola.org>
2024-11-25 23:10:39 -0500

    get new news and create telegraph

--- src/feedrss.py
+++ src/feedrss.py
@@ -8,59 +8,40 @@ for fetching and storing RSS feed data.
 import argparse
 import utils as ut
 from logging_conf import logger
-from typing import Namespace
 
 log = logger.getChild(__name__)
-
-
-def setup_parser() -> Namespace:
-    """
-    Set up and configure command line argument parser.
-
-    Returns:
-        Namespace: Parsed command line arguments
-    """
-    parser = argparse.ArgumentParser(description="Feed RSS")
-    parser.add_argument(
-        "-u",
-        "--url",
-        type=str,
-        help="URL of the RSS feed",
-        required=False
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        help="Output file",
-        required=False
-    )
-
-    try:
-        args = parser.parse_args()
-        log.debug(f"start feedrss with args: {args}")
-
-        # Update environment variables with parsed arguments
-        for key, value in vars(args).items():
-            if key != "version":
-                ut.ENV[key] = value
-
-        return args
-    except Exception as e:
-        log.error(f"Failed to parse arguments: {str(e)}")
-        raise
 
 
 def main() -> None:
     """Main execution function to fetch and store RSS feed data."""
     try:
-        args = setup_parser()
+        import mdb as db
+        import lapresse as lp
+        import rssutils
+        newrss = lp.fetch_rss()
+        lastrss = db.get_last_rss(lp.NAME)
 
-        import lapresse
-        import mdb
+        entities = rssutils.get_new_entries(newrss, lastrss)
+        if entities:
+            log.info(f"New entries found: {len(entities)}")
+            db.save_rss(newrss)
+            for entity in entities:
+                log.info(f"Processing: {
+                         entity['published']}-{entity['title']} \n {entity['link']} \n {entity['image']}")
 
-        lapresse.fetch_rss()
-        mdb.insert_one(lapresse.rss)
+                content = lp.get_entry_content(entity)
+                text = lp.get_readability(content)
+                telegraph_content = lp.prepare_telegraph_content(text)
+                translated_text = lp.translate_content(telegraph_content)
+                title = lp.translate_text(
+                    entity["title"],
+                    source_lang="French",
+                    target_lang="Simple Chinese"
+                )
+                url = lp.create_telegraph_page(title, translated_text)
+                log.info(f"Telegraph URL: {url}")
+        else:
+            log.info("No new entries found")
 
     except Exception as e:
         log.error(f"Error in main execution: {str(e)}")
